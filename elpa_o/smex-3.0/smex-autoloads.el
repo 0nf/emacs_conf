75c86ecdccdf8e836327ec3c8e8041df398eba84
0nf <0nf0r0l@gmail.com>
2016-04-03 02:59:11 +0300

    one more stupid try

---
+++ elpa_o/smex-3.0/smex-autoloads.el
@@ -0,0 +1,33 @@
+;;; smex-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (smex-initialize smex) "smex" "smex.el" (21569
+;;;;;;  39853 101607 963000))
+;;; Generated autoloads from smex.el
+
+(autoload 'smex "smex" "\
+
+
+\(fn)" t nil)
+
+(autoload 'smex-initialize "smex" "\
+
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("smex-pkg.el") (21569 39853 184862 589000))
+
+;;;***
+
+(provide 'smex-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; smex-autoloads.el ends here
